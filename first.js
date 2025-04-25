ce5bf18ed7c0b0b3740e1060d6ad1a6626cddf89
ankit-yadav90 <ankityadav73699@gmail.com>
2025-04-25 10:05:46 +0530

    Larjest num

--- first.js
+++ first.js
@@ -1,7 +1,18 @@
 let arr = [1, 2, 3, 4,];
 
-const output = arr.reduce((res, cur) => {
+let output = arr.reduce((res, cur) => {
     return res + cur;
 })
 
 console.log(output);
+
+
+//To print the larjest number 
+
+let num = [5, 6, 2, 1, 101, 3];
+
+const larjestnum = num.reduce((prev, cur) => {
+    return prev > cur ? prev : cur;
+});
+
+console.log(larjestnum);
